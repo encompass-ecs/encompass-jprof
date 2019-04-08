5dcdd4f1688f75d9e50581f2c43e54531dcbf00c
Evan Hemsley <evan@moonside.games>
2019-04-07 22:18:35 -0700

    stubbed functions

---
+++ index.ts
@@ -0,0 +1,3 @@
+export function push(this: void, name: string, annotation?: any): void {}
+export function pop(this: void, name?: string): void {}
+export function write(this: void, filename: string): void {}
